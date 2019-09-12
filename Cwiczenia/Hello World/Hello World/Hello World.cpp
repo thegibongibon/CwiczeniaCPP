d83e07ca681db5ad45209150291a57997e4c1ef9
Your Name <monika@garmory.pl>
2019-09-12 13:02:30 +0200

    nowe

---
+++ Cwiczenia/Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,37 @@
+﻿// Hello World.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World!\n";
+
+	string imie;
+	cin >> imie;
+	cout << "witaj \n" + imie;
+
+
+	int kroki = 29;
+	double czasLotu = 3.5;
+	char separator = '.';
+	separator = '/';
+	cout << "10" << separator << "07" << separator;
+
+	bool czyKupa = true;
+
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
---
+++ Cwiczenia/alfabet/alfabet/alfabet.cpp
@@ -0,0 +1,33 @@
+﻿// alfabet.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "podaj dwa imiona " << endl;
+	string pierwszeImie, drugieImie;
+	cin >> pierwszeImie >> drugieImie;
+	if (pierwszeImie == drugieImie) {
+		cout << "Imiona sa takie same  \n";
+	}
+	if (pierwszeImie < drugieImie) {
+		cout << pierwszeImie << " jest wczesniej " << drugieImie;
+	}
+	if (pierwszeImie > drugieImie)
+	{
+		cout << pierwszeImie << " jest pozniej " << drugieImie;
+	}
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
